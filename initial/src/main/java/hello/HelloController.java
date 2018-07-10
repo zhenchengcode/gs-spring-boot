9518490d71a3182d6bf529e768d7b1f29f27a463
zhenchengcode <amadeus.zhencheng@gmail.com>
2018-07-10 14:39:27 -0700

    add testcase, test java file needs to be in another separate folder, otherwise cant find test libary

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -3,9 +3,11 @@ package hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// @Controller + @ResponseBody
 @RestController
 public class HelloController {
-    
+
+    // invoked from browser/curl/postman etc.
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
